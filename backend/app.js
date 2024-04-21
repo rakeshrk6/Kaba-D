25c4b400b10cbcee0ebba615762fe5b6c15556e9
Rakesh <krakesh7788@gmail.com>
2024-04-21 22:51:59 +0530

    solved

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-// config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({
-        path:"backend/congig/.env"
-    })
-}
+// // config
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//     require("dotenv").config({
+//         path:"backend/congig/.env"
+//     })
+// }
 
-
-module.exports = app;
+// module.exports = app;
